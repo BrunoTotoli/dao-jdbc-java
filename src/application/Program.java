c067560f8357ea386c6416247293b8f6d565ed1d
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-17 02:36:43 -0300

    DaoFactory and SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,9 +14,8 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection conn = DB.getConnection();
         Department department = new Department(1, "Books");
         Seller seller = new Seller(1, "Moacir", "moacir@gmail.com", sdf.parse("12/04/2002"), 18000D, department);
-        System.out.println(seller);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
     }
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller seller) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public void update(Seller seller) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
